664c4f823a596595b545a22bbbf08220d5fb3ebb
jayuchawla78 <jayprakashchawla78@gmail.com>
2024-01-13 01:43:12 +0530

    edit leads

--- toolcrm/leads/urls.py
+++ toolcrm/leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import add_lead, leads_list, lead_detail, lead_delete
+from .views import add_lead, leads_list, lead_detail, lead_delete, edit_lead
 urlpatterns = [
     path(route='', view=leads_list, name='leads_list'),
     path(route='add_lead/', view=add_lead, name='add_lead'),
     path(route='<int:pk>/', view=lead_detail, name='lead_detail'),
     path(route='<int:pk>/delete/', view=lead_delete, name='lead_delete'),
+    path(route='<int:pk>/edit/', view=edit_lead, name='edit_lead'),
 ]
--- toolcrm/leads/views.py
+++ toolcrm/leads/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.utils import timezone
+from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
@@ -23,7 +26,8 @@ def add_lead(request):
             lead = form.save(commit=False)
             lead.created_by = request.user
             lead.save()
-            return redirect('dashboard')
+            messages.success(request=request, message='Lead created successfully.')
+            return redirect(reverse('leads_list'))
     else:
         form = AddLeadForm()
         return render(request=request, template_name="leads/add_lead.html", context={'form':form})
@@ -33,4 +37,21 @@ def lead_delete(request, pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
     lead_name = "{} {}".format(lead.first_name, lead.last_name)
     lead.delete()
+    messages.success(request=request, message='Lead: {} deleted successfully.'.format(lead_name))
     return redirect(to='leads_list')
+
+@login_required
+def edit_lead(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    form = None
+    if request.method == "POST":
+        form = AddLeadForm(request.POST, instance=lead)
+        lead = form.save(commit=False)
+        lead.modified_at = timezone.now()
+        lead.save()
+        messages.success(request=request, message='Lead: {} updated successfully.'.format(lead.first_name + " " + lead.last_name))
+        return redirect(to=reverse('lead_detail', args=(lead.id,)))
+    else:
+        form = AddLeadForm(instance=lead)
+        return render(request=request, template_name='leads/edit_lead.html', context={'form':form})
+    
