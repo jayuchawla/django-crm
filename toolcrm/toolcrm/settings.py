46c43f7b8baebe6d3859e8ef9d14edb6f79c6fc6
unknown <jayprakashchawla78@gmail.com>
2024-01-14 18:44:29 +0530

    setup local tailwind

--- toolcrm/toolcrm/settings.py
+++ toolcrm/toolcrm/settings.py
@@ -40,13 +40,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
     'core',
     'toolcrm',
     'userprofile',
     'dashboard',
     'leads',
     'clients',
+    'teams',
 ]
+
+TAILWIND_APP_NAME='theme'
+
+INTERNAL_IPS=["127.0.0.1",]
+
+from shutil import which
+NPM_BIN_PATH=which("npm")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'theme/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
